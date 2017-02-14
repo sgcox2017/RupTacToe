df617d42954a97342506d45b3c5818ae975eba7c
sgcox2017 <sgcox2017@spartans.manchester.edu>
2017-02-14 16:47:40 -0500

    Updated the Model
    Made some slight changes to the Board and Player class to suit the needs of the Board Controller.

--- RupTacToe/src/Model/Board.java
+++ RupTacToe/src/Model/Board.java
@@ -1,5 +1,48 @@
 package Model;
 
+import java.io.IOException;
+
 public class Board {
+    
+    private static final int boardSize = 3;
+    private final int rSize;
+    private final int cSize;
+    private final Player player;
+    private Object[][] board;
+    
+    public Board(Player p) throws IOException {
+        this.rSize      = Board.getBoardSize();
+        this.cSize      = this.getBoardSize();
+        this.player     = p;
+    }
+    
+    private static int getBoardSize() {
+        return boardSize;
+    }
+    
+    public void createBoard() {
+        for(int r = 0; r < Board.getBoardSize(); ++r) {
+            for(int c = 0; c < cSize; ++c) {
+                board[r][c] = 'O';
+            }
+        }
+    }
+    
+    public void setup() throws IOException {
+        this.board      = new Character[getrSize()][getcSize()];
+        createBoard();
+    }
+    
+    public int getrSize() {
+        return rSize;
+    }
+    
+    public int getcSize() {
+        return cSize;
+    }
+    
+    public Object[][] getBoard() {
+        return board;
+    }
     
 }
