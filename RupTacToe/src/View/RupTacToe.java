9bc676ca79ec9b56719a5f781b9a4d0d64a6fcba
sgcox2017 <sgcox2017@spartans.manchester.edu>
2017-02-20 06:08:17 -0500

    First Iteration of View Complete and Documented

--- RupTacToe/src/View/RupTacToe.java
+++ RupTacToe/src/View/RupTacToe.java
@@ -3,31 +3,33 @@ package View;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+//The main class which creates the scene and starts the program
 public class RupTacToe extends Application {
+    
+    //The width and height of the scene and the scene itself
+    private final int width  = 1000;
+    private final int height = 700;
+    private static Scene scene;
     
     @Override
     public void start(Stage primaryStage) throws IOException {
        
+        //Creates the scene
         Parent startLayout = FXMLLoader.load(getClass().getResource("TitleScene.fxml"));
         StackPane startScene = new StackPane(startLayout);
-        Scene scene = new Scene(startScene);
+        scene = new Scene(startScene);
         
-        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        primaryStage.setScene(scene);
-        primaryStage.setFullScreen(true);
-        primaryStage.setX(visualBounds.getMinX());
-        primaryStage.setY(visualBounds.getMinY());
-        primaryStage.setWidth(visualBounds.getWidth());
-        primaryStage.setHeight(visualBounds.getHeight());
-        
-        primaryStage.setTitle("Tic Tac Toe");
+        //Sets the sizing and title of the scene and shows it
+        primaryStage.setScene(scene);  
+        primaryStage.setWidth(width);
+        primaryStage.setHeight(height);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("SCRAM Tic-Tac-Toe");
         primaryStage.show();
         
     } 
@@ -36,4 +38,7 @@ public class RupTacToe extends Application {
         launch(args);
     }
     
+    public static Scene getScene() {
+        return scene;
+    }
 }
